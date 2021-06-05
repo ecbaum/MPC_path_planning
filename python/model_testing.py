cf3e968c0b65adaf45947a611b560d4ecf8877bc
ecbaum <carlbaum.ekholm@gmail.com>
2021-06-05 11:24:55 +0200

    Simple MPC working. Error fixed: Discrete dynamics in ODE -> continous dynamics

--- python/model_testing.py
+++ python/model_testing.py
@@ -1,11 +1,10 @@
 from casadi import *
 from matplotlib import pyplot as plt
-T = 5  # time horizon
+T = 20  # time horizon
 N = 50  # Number of control intervals
 
 
 h = T/N
-
 
 x1 = MX.sym('x')  # state variables
 x2 = MX.sym('y')
@@ -19,10 +18,9 @@ u2 = MX.sym('uy')
 
 u = vertcat(u1, u2)
 
-ode = vertcat(h*x3 + x1, h*x4 + x2, x3 + u1, x4 + u2)
+ode = vertcat(h*x3, h*x4, u1, u2)
 
 f = Function('f', [x, u], [ode], ['x', 'u'], ['ode'])
-
 
 
 # Integrator to discretize the system
@@ -38,26 +36,16 @@ x_next = res["xf"]
 
 F = Function('F', [x, u], [x_next], ['x', 'u'], ['x_next'])
 
-
 sim = F.mapaccum(N)
 
-x0 = vertcat(0, 0, 0, 0)
+x0 = vertcat(1, 1, 0.1, 0.1)
 
 #u_list = horzcat(-cos(range(0, N)),sin(range(0, N))).T
-
-u_list = 2*(np.random.rand(2, N)-0.5)
+u_list = np.random.rand(2, N)-0.5
 
 res1 = sim(x0, u_list)
 
 res1 = horzcat(x0, res1).toarray()
-
-#tgrid = np.linspace(0, T, N+1)
-#fig = plt.figure()
-#plt.scatter(res1[0, :], res1[1, :], label='position')
-#plt.plot(tgrid, res[0, :], label='x1')
-#plt.plot(tgrid, res[1, :], label='x2')
-#plt.step(tgrid, vertcat(u_list.toarray(),np.nan)[:, 0], '-.', color='black', label='u')
-#plt.legend()
 
 opti = casadi.Opti()
 
@@ -71,21 +59,27 @@ opti.minimize(sumsqr(x))
 for k in range(N):
     opti.subject_to(x[:, k + 1] == F(x[:, k], u[:, k]))
 
-opti.subject_to(opti.bounded(-1, u, 1))
+opti.subject_to(opti.bounded(-2, u, 2))
 
 opti.subject_to(x[:, 0] == p)
 
 opti.solver('ipopt')
-#opti.solver('sqpmethod', {'qpsol': 'osqp'})
 
-opti.set_value(p, vertcat(10, 10, 0, 0))
+
+opti.set_value(p, vertcat(10, 10, -3, 6))
 sol = opti.solve()
 
 res2 = sol.value(x)
+u2 = sol.value(u)
 
-fig, axs = plt.subplots(4)
-axs[0].plot(res2[0, :])
-axs[1].plot(res2[1, :])
-axs[2].plot(res2[2, :])
-axs[3].plot(res2[3, :])
-#plt.scatter(res2[0, :], res2[1, :], label='position')
+
+tgrid = np.linspace(0, T, N+1)
+
+fig, axs = plt.subplots(3)
+axs[0].plot(tgrid, res2[0], label='x')
+axs[0].step(tgrid, vertcat(u2[0], np.nan), '-.', color='black', label='u_x')
+axs[0].legend()
+axs[1].plot(tgrid, res2[1], label='y')
+axs[1].step(tgrid, vertcat(u2[1], np.nan), '-.', color='black', label='u_y')
+axs[1].legend()
+axs[2].scatter(res2[0, :], res2[1, :], label='position')
